f4a05c126fd0b908836a4fac9bdb148ac7dea9b6
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-08 12:30:19 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <div>
+      <h1>Let's get started ! </h1>
+      <p> this is als visibility: </p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  )
+}
+
+export default App;
---
+++ my-app/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() 
+{
+
+    return  (
+    <div>
+        <div>Date</div>
+        <div>Amount</div>
+        <div>Title</div>
+    </div>
+);
+}
+
+export default ExpenseItem;
