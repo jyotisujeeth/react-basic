c844749a78b10a32cb17a841db1ffd53af9b3d70
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-09 13:13:44 +0530

    Update ExpenseItem.js

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
+import './ExpenseItem.css'
 function ExpenseItem() 
 {
-
-    return  (
-    <div>
+const expenseDate = new Date(2023, 2, 28)
+const expenseAmount = 285.84;
+const expenseTiltle = "car insurance";
+    return (
+      <div className="expense-item">
         <div>Date</div>
         <div>Amount</div>
+        <h2>{Math.random()}</h2>
+        <h2>{expenseTiltle}</h2>
+
         <div>Title</div>
-    </div>
-);
+      </div>
+    );
 }
 
 export default ExpenseItem;
