5a8c4c9670f2a7b746fd0a8167250e8bcfe7653e
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-10 05:32:52 +0530

    Update ExpenseItem.js

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -6,11 +6,16 @@ const expenseAmount = 285.84;
 const expenseTiltle = "car insurance";
     return (
       <div className="expense-item">
-        <div>Date</div>
-        <div>Amount</div>
+        // <div>Date</div>
+        // <div>Amount</div>
+        // <h2>{Math.random()}</h2>
+        // <h2>{expenseTiltle}</h2>
+
+          <div>{expenseDate.toISOString()}</div> 
+        <div>{expenseAmount}</div>
         <h2>{Math.random()}</h2>
         <h2>{expenseTiltle}</h2>
-
+        <div className="expense-item-price">${expenseAmount}</div>
         <div>Title</div>
       </div>
     );
