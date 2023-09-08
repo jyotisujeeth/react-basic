a8f2577c6a48243765b76a281f56303036032214
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-09 04:30:14 +0530

    Update index.js

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -33,3 +33,30 @@ class Header extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Header />);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+The getDerivedStateFromProps method is called right before the render method:
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  static getDerivedStateFromProps(props, state) {
+    return {favoritecolor: props.favcol };
+  }
+  render() {
+    return (
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header favcol="yellow"/>);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+            
