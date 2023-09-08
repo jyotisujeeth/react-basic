049d3096e24c7eb13b06c251ba0d4c45d5bc1113
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-09 04:28:57 +0530

    Create index.js

---
+++ my-app/src/components/index.js
@@ -0,0 +1,35 @@
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  static getDerivedStateFromProps(props, state) {
+    return {favoritecolor: props.favcol };
+  }
+  render() {
+    return (
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header favcol="yellow"/>);
+
+render
+The render() method is required, and is the method that actually outputs the HTML to the DOM.
+
+Example:
+A simple component with a simple render() method:
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>This is the content of the Header component</h1>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header />);
