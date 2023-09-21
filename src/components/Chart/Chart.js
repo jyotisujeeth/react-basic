886825611cf8acbd492148503637cc3a812350d4
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-22 00:11:07 +0530

    Chart task final

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,8 +4,8 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = (props) => {
-  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-  const totalMaximum = Math.max(...dataPointValues);
+  const dataPointValues  = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum= Math.max(...dataPointValues);
 
   return (
     <div className="chart">
