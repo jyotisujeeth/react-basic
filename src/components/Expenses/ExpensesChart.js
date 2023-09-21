15e2bfa1662f6c5fb78fd018dae74e6fa9443f86
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-22 00:06:43 +0530

    Chart task final

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -5,6 +5,7 @@ import Chart from "../Chart/Chart";
 const ExpensesChart = (props) => {
   const chartDataPoints = [
     { label: "Jan", value: 0 },
+
     { label: "Feb", value: 0 },
     { label: "Mar", value: 0 },
     { label: "Apr", value: 0 },
@@ -19,6 +20,7 @@ const ExpensesChart = (props) => {
   ];
 
   for (const expense of props.expenses) {
+    
     const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
     chartDataPoints[expenseMonth].value += expense.amount;
   }
